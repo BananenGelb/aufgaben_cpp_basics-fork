521ef01859c06fd807cd06329f98ce678d8d47f5
BananenGelb <Benedict.lotter@gmx.de>
2025-05-29 18:49:11 +0200

    Update guide.cpp

--- src/tourist_guide_v1/guide.cpp
+++ src/tourist_guide_v1/guide.cpp
@@ -5,6 +5,11 @@ std::pair<std::string, int> get_place_info(
     std::vector<std::string> places, std::vector<std::string> descriptions,
     std::vector<int> entry_fees, std::string place) {
   // TODO
-  return {"", -1};
+  for (int i = 0; i < places.size(); i++){
+    if (places.at(i) == place){
+      return std::make_pair(descriptions.at(i), entry_fees.at(i));
+    }
+  }
+  return std::make_pair("", -1);
 }
 }  // namespace guide
